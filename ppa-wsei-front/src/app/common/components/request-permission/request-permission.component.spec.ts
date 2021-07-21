160ec81b63f69a7cbb751833f401ab507a1914a9
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2021-07-21 21:52:39 +0200

    Create Request Permission component

---
+++ ppa-wsei-front/src/app/common/components/request-permission/request-permission.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RequestPermissionComponent } from './request-permission.component';
+
+describe('RequestPermissionComponent', () => {
+  let component: RequestPermissionComponent;
+  let fixture: ComponentFixture<RequestPermissionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RequestPermissionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RequestPermissionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
