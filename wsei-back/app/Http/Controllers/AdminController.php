157cc1e5ed3c52e43db0f633dc9320aecb013e82
Jakub Sewiło <jakub.sewilo@cm-uj.krakow.pl>
2021-07-22 00:49:07 +0200

    Create AdminController and methods for show users permission requests and handle it

---
+++ wsei-back/app/Http/Controllers/AdminController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PermissionRequest;
+use App\Models\UserPermission;
+use App\Repositories\UserRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
+
+class AdminController extends Controller
+{
+    public function requests()
+    {
+        $requests = PermissionRequest::where('accepted', 0)->get();
+        try {
+            $requests = PermissionRequest::where('accepted', 0)->get();
+
+            return response()->json(['success' => 'success', 'requests' => $requests], 201);
+        } catch (\Exception $e) {
+            Log::error($e);
+            return response()->json(['success' => 'fail'], 404);
+        }
+    }
+
+    public function handleRequest(Request $request)
+    {
+        $this->validate($request, [
+            'is_accepted'    => 'required|boolean',
+            'permission_id' => 'required|integer'
+        ]);
+
+        try {
+            DB::transaction(function () use ($request) {
+                $accepted = $request->input('is_accepted') ? 1 : 2;
+                $permRequest = PermissionRequest::where('id', $request->input('permission_id'))->firstOrFail();
+                $permRequest->accepted = $accepted;
+                $permRequest->save();
+                if ($request->input('is_accepted')) {
+                    $userPermission = UserPermission::where('user_id', $permRequest->user_id)->firstOrFail();
+                    $userPermission->permission = $permRequest->permission;
+                    $userPermission->save();
+                }
+            });
+
+            return response()->json(['success' => 'success'], 201);
+        } catch (\Exception $e) {
+            Log::error($e);
+            return response()->json(['success' => 'fail'], 404);
+        }
+    }
+}
