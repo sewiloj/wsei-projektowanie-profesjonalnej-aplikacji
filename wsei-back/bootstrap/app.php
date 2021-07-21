8144f182acad4ba0cbd2403e881a7bb9dcad51d9
Jakub Sewiło <jakub.sewilo@cm-uj.krakow.pl>
2021-07-21 14:45:02 +0200

    Add CorsMiddleware

--- wsei-back/bootstrap/app.php
+++ wsei-back/bootstrap/app.php
@@ -80,6 +80,10 @@ $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
 ]);
 
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
+
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
