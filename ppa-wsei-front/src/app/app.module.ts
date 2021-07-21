a7b8944dd81ce853972042cca866e0fbb3216e0f
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2021-07-21 16:29:57 +0200

    Add sidenav and content container

--- ppa-wsei-front/src/app/app.module.ts
+++ ppa-wsei-front/src/app/app.module.ts
@@ -1,29 +1,34 @@
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LoginModule } from './login/login.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { ToastrModule } from 'ngx-toastr';
 import { WseiCommonModule } from './common/wsei-common.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+/**
+ * Angular material imports.
+ */
+const MATERIAL_IMPORTS = [MatSidenavModule];
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule,
     LoginModule,
-    CommonModule,
-    ToastrModule.forRoot(),
     WseiCommonModule,
+    MATERIAL_IMPORTS,
+    ToastrModule.forRoot(),
     FontAwesomeModule,
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
