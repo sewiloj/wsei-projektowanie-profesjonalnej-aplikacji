8e4e9a4d42541d34afd3fb61db20ddedebdb66b6
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2021-07-22 15:00:32 +0200

    Close the role dialog after successful submit

--- ppa-wsei-front/src/app/common/components/role-dialog/role-dialog.component.ts
+++ ppa-wsei-front/src/app/common/components/role-dialog/role-dialog.component.ts
@@ -1,10 +1,9 @@
-import { AuthService } from 'src/app/login/services/auth.service';
-
-import { ToastrService } from 'ngx-toastr';
-import { UserPermission, USER_PERMISSIONS } from './../../models/user-permission';
 import { Component } from '@angular/core';
-import { UserType } from 'src/app/login/models/user-type';
+import { MatDialogRef } from '@angular/material/dialog';
 import { RoleService } from '../../services/role.service';
+import { ToastrService } from 'ngx-toastr';
+import { USER_PERMISSIONS, UserPermission } from './../../models/user-permission';
+import { UserType } from 'src/app/login/models/user-type';
 
 @Component({
   templateUrl: './role-dialog.component.html',
@@ -16,16 +15,33 @@ export class RoleDialogComponent {
    */
   public userPermissions: UserPermission[] = USER_PERMISSIONS;
 
-  constructor(private roleService: RoleService, private toastService: ToastrService) {}
+  constructor(
+    private dialogRef: MatDialogRef<RoleDialogComponent>,
+    private roleService: RoleService,
+    private toastService: ToastrService
+  ) {}
 
   /**
    * Request permission for the user.
    */
   public submitRole(permission: UserType, message: string) {
     this.roleService.requestPermission(permission, message).subscribe({
-      next: (state) => (state ? this.toastService.success('Operation was successful') : this.onError()),
+      next: (state) => this.handleSubmitRole(state),
       error: () => this.onError(),
     });
+  }
+
+  /**
+   * Handle the request from submit role.
+   * @param state Whether the operation was successful or not.
+   */
+  private handleSubmitRole(state: boolean) {
+    if (state) {
+      this.toastService.success('Operation was successful');
+      this.dialogRef.close();
+    } else {
+      this.onError();
+    }
   }
 
   /**
